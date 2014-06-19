346ee29cef1e7af3a0fd08a2283b3d408ff9e073
root <root@localhost.localdomain>
2014-06-19 18:10:07 -0500

    Added a config file that can be set locally that should contain the output device. Also adds a quirky demo

--- lcdmusic.py
+++ lcdmusic.py
@@ -1,10 +1,39 @@
 from subprocess import call
+from time import sleep
 
-outputLocation = "testFile.txt"
+file = open("outputDevice.txt", "r")
+outputLocation = file.read()
 
-message = "'Hello'"
-
+blank = "'                                        '"
 command = "echo"
+line1 = ""
+line2 = ""
 
-call(command + " " + message + " > " + outputLocation, shell=True)
+switch = True
+
+slide1a = "'(O.O)  (*.*)  ($.$) '"
+slide2a = "'(-.-)  (0.O)  (O.O) '"
+
+slide1b = "'*-*-*-*-*-*-*-*-*-*-'"
+slide2b = "'-*-*-*-*-*-*-*-*-*-*'"
+
+while True:
+	
+	message = blank
+	call(command + " " + message + " > " + outputLocation, shell=True)
+
+	if switch:
+		message = slide1a
+		message += slide1b
+		switch = False
+	else:
+		message = slide2a
+		message += slide2b
+		switch = True
+
+	call(command + " " + message + " > " + outputLocation, shell=True)
+
+	sleep(0.01)
+
+
 
